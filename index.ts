1979557592cf1add50448d3957a0dd9e83cad8d6
sam primeaux <inneranimalclothing@gmail.com>
2026-04-09 17:13:07 -0500

    Add barrel export for GorillaModeShell

---
+++ index.ts
@@ -0,0 +1,6 @@
+/**
+ * src/core/gorilla-shell/index.ts
+ * Barrel export for GorillaModeShell
+ */
+export { GorillaModeShell } from './GorillaModeShell';
+export type { GorillaModeHandle, GorillaModeProps, ShellTab } from './GorillaModeShell';
